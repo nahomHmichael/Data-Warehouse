b0614905b336d705984033deef8bc33102920760
nahomHmichael <nahomhabtemichael@gmail.com>
2022-11-12 16:44:07 +0300

    fixes + added clean task

--- airflow/dags/postgre_create_table.py
+++ airflow/dags/postgre_create_table.py
@@ -1,10 +1,15 @@
 import airflow
+import os,sys
+import pandas as pd
+
 from sqlalchemy import create_engine
 from datetime import timedelta,datetime
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-import os
-import pandas as pd
+
+sys.path.append(os.path.abspath(".."))
+
 
 default_args = {
     'owner':'nahom',
@@ -12,13 +17,37 @@ default_args = {
     'retry_delay':timedelta(minutes=2)
 }
 
-def load_raw_data(path, table_name):
+def load_clean_data(path, table_name):
+    """task to load clean data to postgres database
+
+    Args:
+        path (str): path to clean data 
+        table_name (str): database table name
+    """
     print("writing data..............")
     engine = create_engine("postgresql+psycopg2://airflow:airflow@postgres/airflow", echo=True)
     df = pd.read_csv(path, sep=",", index_col=False)
 
     df.to_sql(table_name, con=engine, if_exists='replace',index_label='id')
     print("!!!!Done!!!!")
+    
+def clean_df(path):
+    """function to clean raw data 
+
+    Args:
+        path (str): path of the data to be cleaned
+    """
+    
+    df = pd.read_csv(path,sep='[;:]',index_col=False)
+    print('data loaded successfully!!!')
+    df.columns = df.columns.str.replace(' ','')
+    try:
+        df.to_csv('./data/cleanned_data.csv',index=False)
+    except Exception as e:
+        print(f'error: {e}')
+    
+
+
 
 
 with DAG(
@@ -29,14 +58,22 @@ with DAG(
     schedule_interval='@once'
 )as dag:
     task1 = PythonOperator(
-        task_id='load_raw_data',
-        python_callable=load_raw_data,
+        task_id='load_clean_data',
+        python_callable=load_clean_data,
         op_kwargs={
-            "path": "./data/drone3_clean.csv",
+            "path": "./data/cleanned_data.csv",
             "table_name":"traffic_raw"
         }
     )
-    task1
+    task_2 = PythonOperator(
+        task_id='extract_and_clean_data',
+        python_callable=clean_df,
+        op_kwargs={
+            "path":"./data/drone3.csv"}
+    )
+    task_2>>task1
+    
+    
 # def read_data():
 #     df = pd.read_csv('/opt/airflow/data/drone3_clean.csv')
     
